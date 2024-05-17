7920ee60cefbcc3250a69ea9a227b422df14e7b5
Violoncello <100255591+SassyAssSas@users.noreply.github.com>
2024-05-17 19:20:40 +0200

    Floor generation update
    Floor can now contain 2x2 pieces

--- dream-rogue/Assets/Scripts/Game/Entities/Level/States/LevelGenerationState.cs
+++ dream-rogue/Assets/Scripts/Game/Entities/Level/States/LevelGenerationState.cs
@@ -1,24 +1,26 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
 using Violoncello.Services;
 
 namespace SecretHostel.DreamRogue {
    public class LevelGenerationState : LevelViewModel.State {
       private LevelGenerationStateConfig _config;
 
-      private Dictionary<string, GameObject> _floorTiles = new();
+      private Dictionary<string, GameObject> floorTilesPrefabs = new();
 
-      private readonly Vector3 FloorTileSize = new(1f, 1f, 0.5f);
+      private const string fullTileKey = "full";
+      private const string cornerTileKey = "corner";
 
       private LevelGenerationState(LevelViewModel viewModel, LevelGenerationStateConfig config, IAssetsLoader assetsLoader) : base(viewModel) {
          _config = config;
 
-         var tile = assetsLoader.Load<GameObject>("Models", "floor-tile-full");
+         var full = assetsLoader.Load<GameObject>("Models", "floor-tile-full");
+         var corner = assetsLoader.Load<GameObject>("Models", "floor-tile-corner");
 
-         _floorTiles = new() {
-            { "1x1", tile }
+         floorTilesPrefabs = new() {
+            { fullTileKey, full },
+            { cornerTileKey, corner }
          };
       }
 
@@ -32,53 +34,120 @@ namespace SecretHostel.DreamRogue {
       private void SpawnRoom(Room room, Vector2 position) {
          var offset = position - room.Size / 2;
 
+         var floor = new GameObjectTile[room.Size.x, room.Size.y];
+
+         CreateFloor1x1TilesNoAlloc(floor);
+         CreateFloor2x2TilesNoAlloc(floor);
+        
          for (int i = 0; i < room.Size.x; i++) {
             for (int j = 0; j < room.Size.y; j++) {
-               var x = i + offset.x;
-               var z = j + offset.y;
+               var tile = floor[i, j];
+               var tilePosition = new Vector3(i + offset.x, 0f, j + offset.y);
 
-               PlaceTile(new Vector3(x, 0, z), _floorTiles["1x1"]);
+               PlaceTile(tile.Prefab, tilePosition, Quaternion.Euler(tile.EulerRotation));
             }
          }
+      }
+
+      private void CreateFloor1x1TilesNoAlloc(GameObjectTile[,] floor) {
+         var width = floor.GetLength(0);
+         var height = floor.GetLength(1);
+
+         for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+               floor[x, y] = new GameObjectTile(floorTilesPrefabs[fullTileKey], new Vector2Int(x, y), new Vector3(270f, 0f, 0f));
+            }
+         }
+      }
+
+      private void CreateFloor2x2TilesNoAlloc(GameObjectTile[,] floor) {
+         var width = floor.GetLength(0);
+         var height = floor.GetLength(1);
+
+         var bigTilesAmount = Mathf.RoundToInt(width * height / 4f * 0.25f);
+
+         var possiblePositions = new List<Vector2Int>();
+
+         for (int x = 0; x < width - 1; x++) {
+            for (int y = 0; y < height - 1; y++) {
+               possiblePositions.Add(new(x, y));
+            }
+         }
+
+         for (int i = 0; i < bigTilesAmount; i++) {
+            if (possiblePositions.Count == 0) {
+               break;
+            }
+
+            var bottomLeftPosition = possiblePositions[Random.Range(0, possiblePositions.Count)];
+
+            for (int x = bottomLeftPosition.x - 1; x <= bottomLeftPosition.x + 1; x++) {
+               for (int y = bottomLeftPosition.y - 1; y <= bottomLeftPosition.y + 1; y++) {
+                  possiblePositions.Remove(new(x, y));
+               }
+            }
+
+            floor[bottomLeftPosition.x, bottomLeftPosition.y] = new GameObjectTile(floorTilesPrefabs[cornerTileKey], bottomLeftPosition, new Vector3(270f, 270f, 0f));
+            floor[bottomLeftPosition.x + 1, bottomLeftPosition.y] = new GameObjectTile(floorTilesPrefabs[cornerTileKey], bottomLeftPosition, new Vector3(270f, 180f, 0f));
+            floor[bottomLeftPosition.x, bottomLeftPosition.y + 1] = new GameObjectTile(floorTilesPrefabs[cornerTileKey], bottomLeftPosition, new Vector3(270f, 0f, 0f));
+            floor[bottomLeftPosition.x + 1, bottomLeftPosition.y + 1] = new GameObjectTile(floorTilesPrefabs[cornerTileKey], bottomLeftPosition, new Vector3(270f, 90f, 0f));
+         }
+      }
+
+      private void GenerateFloorExits(Room room, Vector2 position) {
+         var prefab = floorTilesPrefabs[fullTileKey];
+         var rotation = Quaternion.Euler(270f, 0f, 0f);
 
          if (room.Exits.HasFlag(Exits.Top)) {
             var x = position.x;
             var z = position.y + room.Size.y / 2;
 
-            PlaceTile(new Vector3(x, 0, z), _floorTiles["1x1"]);
-            PlaceTile(new Vector3(x - 1, 0, z), _floorTiles["1x1"]);
+            PlaceTile(prefab, new Vector3(x, 0, z), rotation);
+            PlaceTile(prefab, new Vector3(x - 1, 0, z), rotation);
          }
 
          if (room.Exits.HasFlag(Exits.Bottom)) {
             var x = position.x;
             var z = position.y - room.Size.y / 2 - 1;
 
-            PlaceTile(new Vector3(x, 0, z), _floorTiles["1x1"]);
-            PlaceTile(new Vector3(x - 1, 0, z), _floorTiles["1x1"]);
+            PlaceTile(prefab, new Vector3(x, 0, z), rotation);
+            PlaceTile(prefab, new Vector3(x - 1, 0, z), rotation);
          }
 
          if (room.Exits.HasFlag(Exits.Left)) {
             var x = position.x - room.Size.x / 2 - 1;
             var z = position.y;
 
-            PlaceTile(new Vector3(x, 0, z), _floorTiles["1x1"]);
-            PlaceTile(new Vector3(x, 0, z - 1), _floorTiles["1x1"]);
+            PlaceTile(prefab, new Vector3(x, 0, z), rotation);
+            PlaceTile(prefab, new Vector3(x, 0, z - 1), rotation);
          }
 
          if (room.Exits.HasFlag(Exits.Right)) {
             var x = position.x + room.Size.x / 2;
             var z = position.y;
 
-            PlaceTile(new Vector3(x, 0, z), _floorTiles["1x1"]);
-            PlaceTile(new Vector3(x, 0, z - 1), _floorTiles["1x1"]);
+            PlaceTile(prefab, new Vector3(x, 0, z), rotation);
+            PlaceTile(prefab, new Vector3(x, 0, z - 1), rotation);
          }
       }
 
-      private void PlaceTile(Vector3 position, GameObject prefab) {
-         var tile = UnityEngine.Object.Instantiate(prefab, GroundTilemap.transform);
+      private void PlaceTile(GameObject prefab, Vector3 position, Quaternion rotation) {
+         var tile = Object.Instantiate(prefab, position, rotation, GroundTilemap.transform);
 
-         tile.transform.localScale = FloorTileSize;
-         tile.transform.position = position;
+         tile.transform.localScale = Vector3.one;
+         tile.isStatic = true;
+      }
+
+      private class GameObjectTile {
+         public Vector3 EulerRotation { get; }
+         public Vector2Int GridPosition { get; }
+         public GameObject Prefab { get; }
+
+         public GameObjectTile(GameObject prefab, Vector2Int gridPosition, Vector3 eulerRotation) {
+            GridPosition = gridPosition;
+            Prefab = prefab;
+            EulerRotation = eulerRotation;
+         }
       }
 
       private class Room {
@@ -101,7 +170,7 @@ namespace SecretHostel.DreamRogue {
          }
       }
 
-      [Flags]
+      [System.Flags]
       private enum Exits { 
          Top = 1,
          Bottom = 2,
